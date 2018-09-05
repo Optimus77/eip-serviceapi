960d592d23f65cfb13fecf34607f1cb7efb44b30
jiangfw <jiangfw@inspur.com>
2018-09-05 16:06:02 +0800

    initial

--- src/main/java/com/inspur/icp/EipServiceApplication.java
+++ src/main/java/com/inspur/icp/EipServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IcpDemoApplication {
+public class EipServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IcpDemoApplication.class, args);
+        SpringApplication.run(EipServiceApplication.class, args);
     }
 }
--- src/test/java/com/inspur/icp/EipServiceApplicationTests.java
+++ src/test/java/com/inspur/icp/EipServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IcpDemoApplicationTests {
+public class EipServiceApplicationTests {
 
     @Test
     public void contextLoads() {
