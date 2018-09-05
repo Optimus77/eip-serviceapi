17dd9598b1c00813023564ceb784d8e58b484695
jiangfw <jiangfw@inspur.com>
2018-09-05 21:41:10 +0800

    add controller class

---
+++ src/main/java/com/inspur/eip/controller/EipController.java
@@ -0,0 +1,10 @@
+package com.inspur.eip.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/eip")
+@RestController
+
+public class EipController {
+}
