d15e662ebdc977be9cacee30f5da04564ea258aa
jiasirui <jiasirui@inspur.com>
2018-09-17 15:31:58 +0800

    EIP数据表添加字段 firewall_id floating_ipv6_id floating_ipv4_id 及数据长度定义

--- src/main/java/com/inspur/eip/entity/Eip.java
+++ src/main/java/com/inspur/eip/entity/Eip.java
@@ -17,53 +17,62 @@ public class Eip implements Serializable {
     @Id
     @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     @GeneratedValue(generator = "system-uuid")
-    @Column(name ="eip_id",nullable = false, insertable = false, updatable = false)
-    private String id;
+    @Column(name ="eip_id",nullable = false, insertable = false, updatable = false,length = 36)
+    private String id;//the eip id ,generate by hibernate uuid strategy
 
-    @Column(name="eip_name")
-    private String name;
+    @Column(name="eip_name" ,length = 128)
+    private String name;//the eip name
 
-    @Column(name="elastic_ipv4")
-    private String eipIpv4;
+    @Column(name="elastic_ipv4",columnDefinition="char(16)")
+    private String eipIpv4;//the public ipv4 address on internet
 
-    @Column(name="floating_ipv4")
-    private String floatingIpv4;
+    @Column(name="floating_ipv4",columnDefinition="char(16)")
+    private String floatingIpv4;//the floating ipv4 address on openstack
 
-    @Column(name="fixed_ipv4")
-    private String fixedIpv4;
+    @Column(name="fixed_ipv4",columnDefinition="char(16)")
+    private String fixedIpv4;//the fixed ipv4 address on openstack
 
-    @Column(name="elastic_ipv6")
-    private String eipIpv6;
+    @Column(name="elastic_ipv6",columnDefinition="char(40)")
+    private String eipIpv6;//the  public ipv6 address on internet
 
-    @Column(name="floating_ipv6")
-    private String floatingIpv6;
+    @Column(name="floating_ipv6",columnDefinition="char(40)")
+    private String floatingIpv6;//the floating ipv6 address on openstack
 
-    @Column(name="fixed_ipv6")
-    private String fixedIpv6;
+    @Column(name="fixed_ipv6",columnDefinition="char(40)")
+    private String fixedIpv6;//the fixed ipv6 address on openstack
 
-    @Column(name="instance_id")
-    private String instanceId;
+    @Column(name="floating_ipv4_id",columnDefinition="varchar(36)")
+    private String floatingIpv4Id;
 
-    @Column(name="instance_type")
-    private String instanceType;
+    @Column(name="floating_ipv6_id",columnDefinition="varchar(36)")
+    private String floatingIpv6Id;
 
-    @Column(name="vpc_id")
-    private String vpcId;
+    @Column(name="firewall_id",columnDefinition="varchar(36)")
+    private String firewallId;
 
-    @Column(name="bandwidth")
-    private String banWidth;
+    @Column(name="instance_id",columnDefinition="varchar(36)")
+    private String instanceId;//the instance id of  the fixed ip address banded
 
-    @Column(name="link_type")
-    private String linkType;
+    @Column(name="instance_type",columnDefinition="char(1)")
+    private String instanceType;//the instance type  1：ecs 2：cps 3：slb
 
-    @Column(name="shared_bandwidth_id")
-    private String sharedBandWidthId;
+    @Column(name="vpc_id",columnDefinition="varchar(36)")
+    private String vpcId;//the id  of  vpc that ecs instance  belong to
 
-    @Column(name="acl_id")
+    @Column(name="bandwidth",columnDefinition="varchar(10)")
+    private String banWidth;//the eip bandWidth
+
+    @Column(name="link_type",columnDefinition="char(1)")
+    private String linkType;//eip link type 1.Unicom 2.China Mobile 3 China Telecom
+
+    @Column(name="shared_bandwidth_id",columnDefinition="varchar(36)")
+    private String sharedBandWidthId;//the id of sharedbandwidth
+
+    @Column(name="acl_id",columnDefinition="varchar(36)")
     private String aclId;
 
-    @Column(name="state",nullable = false)
-    private String stat="0";
+    @Column(name="state",nullable = false,columnDefinition="char(1)")
+    private String stat="0";//state 0  1 2 3
 
     @Column(name="create_time" ,nullable = false)
     private Date createTime=new Date();
