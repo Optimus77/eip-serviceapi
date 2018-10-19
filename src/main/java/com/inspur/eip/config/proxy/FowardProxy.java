d9ad8bf14b03f611aa57f4c6dc8ba6304b17cac5
jiasirui <jiasirui@inspur.com>
2018-10-19 17:11:58 +0800

    去除无用jar包及配置

--- src/main/java/com/inspur/eip/config/proxy/FowardProxy.java
+++ src/main/java/com/inspur/eip/config/proxy/FowardProxy.java
@@ -1,21 +1,12 @@
 package com.inspur.eip.config.proxy;
 
-import com.alibaba.fastjson.JSONObject;
-import com.inspur.eip.config.interceptor.CustomInterceptor;
-import com.inspur.eip.config.proxy.httppool.HttpClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.EnvironmentAware;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import java.io.IOException;
