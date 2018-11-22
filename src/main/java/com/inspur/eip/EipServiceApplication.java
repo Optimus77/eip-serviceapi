2e9635e9374b149b9fc2548c84239a4ec5135717
Zerah <muning@inspur.com>
2018-11-22 19:05:44 +0800

    decrease interceptor WebConf And CrosFilter

--- src/main/java/com/inspur/eip/EipServiceApplication.java
+++ src/main/java/com/inspur/eip/EipServiceApplication.java
@@ -1,11 +1,8 @@
 package com.inspur.eip;
 
-import com.inspur.eip.filter.CrosControllerFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"com.inspur"})
 @ServletComponentScan
@@ -15,13 +12,5 @@ public class EipServiceApplication  {
         SpringApplication.run(EipServiceApplication.class, args);
 //        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_THREADLOCAL);
     }
-    @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        CrosControllerFilter crosControllerFilter = new CrosControllerFilter();
-        registrationBean.setFilter(crosControllerFilter);
-        return registrationBean;
-    }
-
 
 }
