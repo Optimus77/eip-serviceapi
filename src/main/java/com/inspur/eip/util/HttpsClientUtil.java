03852911d90e3e431318b5d24650ab84c79af3b0
luhailong <luhailong@inspur.com>
2018-11-29 13:07:29 +0800

    bss interface use http in devops. use https in pro

--- src/main/java/com/inspur/eip/util/HttpsClientUtil.java
+++ src/main/java/com/inspur/eip/util/HttpsClientUtil.java
@@ -16,6 +16,7 @@ import javax.net.ssl.SSLSocket;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -48,7 +49,7 @@ public class HttpsClientUtil {
 
 	static boolean ignoreSSL = Boolean.TRUE;
 
-	public static ReturnResult doGet(String url, Map<String, String> param) {
+	public static HttpResponse doGet(String url, Map<String, String> param) throws  EipException{
 
 		// 创建Httpclient对象
 		CloseableHttpClient httpclient = getHttpsClient();
@@ -72,11 +73,7 @@ public class HttpsClientUtil {
 
 			// 执行请求
 			response = httpclient.execute(httpGet);
-			log.info("response status code: " + response.getStatusLine().getStatusCode());
-
-			resultString = EntityUtils.toString(response.getEntity(), "UTF-8");
-
-			return ReturnResult.actionResult(resultString, response.getStatusLine().getStatusCode());
+			return response;
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -89,15 +86,15 @@ public class HttpsClientUtil {
 				e.printStackTrace();
 			}
 		}
-		return ReturnResult.actionFailed("Exception in get request.", HttpStatus.SC_INTERNAL_SERVER_ERROR);
+		throw new EipException("Post request throw https error.", HttpStatus.SC_INTERNAL_SERVER_ERROR);
 	}
 
-	public static ReturnResult doGet(String url) {
+	public static HttpResponse doGet(String url) throws EipException {
 
 		return doGet(url, null);
 	}
 
-	public static String doPost(String url, Map<String, String> param) {
+	public static HttpResponse doPost(String url, Map<String, String> param) throws  EipException{
 
 		// 创建Httpclient对象
 		CloseableHttpClient httpClient = getHttpsClient();
@@ -119,7 +116,7 @@ public class HttpsClientUtil {
 			// 执行http请求
 			response = httpClient.execute(httpPost);
 			log.info("response status code: " + response.getStatusLine().getStatusCode());
-			resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+			return response;
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -133,19 +130,18 @@ public class HttpsClientUtil {
 
 			}
 		}
-
-		return resultString;
+		throw new EipException("Post request throw https error.", HttpStatus.SC_INTERNAL_SERVER_ERROR);
 	}
 	   
 
-	public static String doPost(String url) {
+	public static HttpResponse doPost(String url) throws EipException{
 
 		return doPost(url, null);
 	}
 
 
 	    
-	public static ReturnResult doPostJson(String url, Map<String, String> header, String json) {
+	public static HttpResponse doPostJson(String url, Map<String, String> header, String json) throws EipException {
 
 		// 创建Httpclient对象
 		CloseableHttpClient httpClient = getHttpsClient();
@@ -170,9 +166,7 @@ public class HttpsClientUtil {
 			// 执行http请求
 			response = httpClient.execute(httpPost);
 			log.info("response status code: " + response.getStatusLine().getStatusCode());
-
-			resultString = EntityUtils.toString(response.getEntity(), "utf-8");
-			return ReturnResult.actionResult(resultString, response.getStatusLine().getStatusCode());
+			return response;
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -185,7 +179,7 @@ public class HttpsClientUtil {
 				e.printStackTrace();
 			}
 		}
-        return ReturnResult.actionFailed("Exception in post request.", HttpStatus.SC_INTERNAL_SERVER_ERROR);
+		throw new EipException("Post request throw https error.", HttpStatus.SC_INTERNAL_SERVER_ERROR);
 	}
 
 	private static Map<String,String> getHeader(){
@@ -196,14 +190,6 @@ public class HttpsClientUtil {
 		return header;
 	}
 
-    /**
-     * get HttpsClient
-     *
-     * @return org.apache.http.impl.client.CloseableHttpClient
-     * @Exception
-     * @auther: zhang_xiaoyu
-     * @date: 2018/9/13 19:09
-     */
     public static CloseableHttpClient getHttpsClient() {
 
         CloseableHttpClient httpClient;
