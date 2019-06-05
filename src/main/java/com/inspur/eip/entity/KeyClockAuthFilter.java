d2051d18acb077b13426f6dbc156eee2a0bce425
Wx0liuys <wx0liuys@inspur.com>
2019-06-05 09:57:36 +0800

    Merge branch 'dev' of git.inspur.com:cloudboss/eip/eip-serviceapi into dev
    # Conflicts:
    #	src/main/java/com/inspur/eip/controller/EipOrderController.java
    #	src/main/java/com/inspur/eip/controller/SbwOrderController.java
    #	src/main/java/com/inspur/eip/controller/v2/EipControllerV2.java
    #	src/main/java/com/inspur/eip/controller/v2/EipOrderControllerV2.java
    #	src/main/java/com/inspur/eip/controller/v2/SbwOrderControllerV2.java
    #	src/main/java/com/inspur/eip/util/v2/CommonUtil.java

---
+++ src/main/java/com/inspur/eip/entity/KeyClockAuthFilter.java
@@ -0,0 +1,64 @@
+package com.inspur.eip.entity;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.inspur.eip.config.CodeInfo;
+import com.inspur.eip.service.impl.EipServiceImpl;
+import com.inspur.eip.util.HsConstants;
+import com.inspur.eip.util.ReturnStatus;
+import com.inspur.eip.util.CommonUtil;
+import com.inspur.icp.common.util.Base64Util;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter
+@Slf4j
+public class KeyClockAuthFilter implements Filter {
+
+    @Autowired
+    private EipServiceImpl eipService;
+
+    @Override
+    public void init(FilterConfig filterConfig)  {
+        log.info("******************KeyClockAuthFilter init");
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)throws IOException, ServletException {
+        HttpServletRequest req= (HttpServletRequest)servletRequest;
+        HttpServletResponse response=(HttpServletResponse)servletResponse;
+        String method =  req.getMethod();
+        log.debug("requtst:{}, {}",method ,  req.getPathInfo());
+        if(req.getHeader("authorization") == null){
+            log.info("get authorization is null ");
+
+            JSONObject result=new JSONObject();
+            result.put("code", ReturnStatus.SC_FORBIDDEN);
+            result.put("message", CodeInfo.getCodeMessage(CodeInfo.KEYCLOAK_NULL));
+            result.put("data",null);
+
+            response.setStatus(HttpStatus.SC_BAD_REQUEST);
+            response.setContentType(HsConstants.APPLICATION_JSON);
+            response.getWriter().write(result.toJSONString());
+        }else{
+            String token = req.getHeader("Authorization");
+            log.debug("get authorization {}",token);
+            CommonUtil.setKeyClockInfo(Base64Util.decodeUserInfo(token));
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("******************KeyClockAuthFilter destroy");
+
+    }
+}
