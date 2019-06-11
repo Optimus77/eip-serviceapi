a1d4775cc14084e659c5e2344c6c48ddd1c87b49
Zerah <muning@inspur.com>
2019-06-11 17:02:39 +0800

    EipReturnDetail

--- src/main/java/com/inspur/eip/entity/ReturnSbwMsg.java
+++ src/main/java/com/inspur/eip/entity/ReturnSbwMsg.java
@@ -1,10 +1,14 @@
 package com.inspur.eip.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Builder
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReturnSbwMsg<T> {
     private String code;
     private String message;
