986205493168da5076c6d03147c0fbcc1b221636
桑新靖 <sangxinjing@inspur.com>
2019-06-14 14:57:11 +0800

    增加atom/eip

--- src/main/java/com/inspur/eip/config/VersionConstant.java
+++ src/main/java/com/inspur/eip/config/VersionConstant.java
@@ -3,4 +3,5 @@ package com.inspur.eip.config;
 public final class VersionConstant {
     public static final String REST_VERSION_1_1 = "/v1.1";
     public static final String REST_VERSION_1_0 = "/v1";
+    public static final String REST_VERSION_1_1ATOM = "atom/v1.1";
 }
