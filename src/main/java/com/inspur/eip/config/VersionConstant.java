94138d5fab67af161f4bb4140cba8fb815724d83
Wx0liuys <wx0liuys@inspur.com>
2019-06-17 13:13:13 +0800

    v2 update v1.1

--- src/main/java/com/inspur/eip/config/VersionConstant.java
+++ src/main/java/com/inspur/eip/config/VersionConstant.java
@@ -1,7 +1,7 @@
 package com.inspur.eip.config;
 
 public final class VersionConstant {
-    public static final String REST_VERSION_1_1 = "/v2";
+    public static final String REST_VERSION_1_1 = "/v1.1";
     public static final String REST_VERSION_1_0 = "/v1";
     public static final String REST_VERSION_1_1ATOM = "atom/v1.1";
 }
